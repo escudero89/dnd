37348909546c8d10ad98635bec26474f75c75b85
Cristian Escudero <cristian.escudero@mercadolibre.com>
2018-06-16 23:15:31 -0300

    Add antd and prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import 'antd/dist/antd.css';
 import './App.css';
+
+import Button from 'antd/lib/button';
 
 class App extends Component {
   render() {
@@ -12,6 +16,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button type="primary">Button</Button>
         </p>
       </div>
     );
---
+++ src/components/Grid/index.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const Grid = () => (
+  <Row gutter={8}>
+    <Col span={8} />
+    <Col span={8} />
+    <Col span={8} />
+  </Row>
+);
